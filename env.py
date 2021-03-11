44307f6f33313a4e86c398d799b400ee7b9474a8
Ubuntu <ubuntu@ip-172-31-39-55.eu-west-1.compute.internal>
2021-03-11 19:57:36 +0000

    New file created to provide a secure repo for sensitive data.

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("SECRET", 'India1971pbf')
+
+os.environ.setdefault("MONGO_URI",'mongodb+srv://Spagettileg:India1971pbf'
+                                  '@myfirstcluster-52qvv.mongodb.net/task_'
+                                  'manager?retryWrites=true&w=majority')
